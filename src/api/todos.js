a087b69b84525f5e01f4549df2352df8b31a753e
Leo <hank8142000@yahoo.com.tw>
2023-07-04 22:24:46 +0800

    getTodos&createTodo API

---
+++ src/api/todos.js
@@ -0,0 +1,26 @@
+import axios from 'axios';
+const baseUrl = 'http://localhost:3001';
+export const getTodos = async () => {
+  try {
+    const res = await axios.get(`${baseUrl}/todos`);
+
+    return res.data;
+  } catch (error) {
+    console.log('[Get Todos failed]', error);
+  }
+};
+export const createTodo = async (payload) => {
+  const { title, isDone } = payload;
+  try {
+    const res = await axios.post(`${baseUrl}/todos`, {
+      title,
+      isDone,
+    });
+
+    return res.data;
+  } catch (error) {
+    console.log('[Post Todo failed]', error);
+  }
+};
+export const patchTodo = () => {};
+export const deleteTodo = () => {};
--- src/pages/TodoPage.jsx
+++ src/pages/TodoPage.jsx
@@ -1,73 +1,70 @@
 import { Footer, Header, TodoCollection, TodoInput } from 'components';
-import { useState } from 'react';
-
-const dummyTodos = [
-  {
-    title: 'Learn react-router',
-    isDone: true,
-    id: 1,
-  },
-  {
-    title: 'Learn to create custom hooks',
-    isDone: false,
-    id: 2,
-  },
-  {
-    title: 'Learn to use context',
-    isDone: true,
-    id: 3,
-  },
-  {
-    title: 'Learn to implement auth',
-    isDone: false,
-    id: 4,
-  },
-];
+import { useEffect, useState } from 'react';
+import { getTodos, createTodo } from '../api/todos';
+import axios from 'axios';
 
 const TodoPage = () => {
   const [inputValue, setInputValue] = useState('');
-  const [todos, setTodos] = useState(dummyTodos);
+  const [todos, setTodos] = useState([]);
 
   function handleChange(value) {
     setInputValue(value);
   }
 
-  function handleAddTodo() {
+  async function handleAddTodo() {
     if (inputValue.length === 0) {
       return;
     }
 
-    setTodos((prevTodos) => {
-      return [
-        ...prevTodos,
-        {
-          title: inputValue,
-          isDone: false,
-          id: Math.random() * 100,
-        },
-      ];
-    });
+    try {
+      const data = await createTodo({
+        title: inputValue,
+        idDone: false,
+      });
 
-    setInputValue('');
+      setTodos((prevTodos) => {
+        return [
+          ...prevTodos,
+          {
+            title: data.title,
+            isDone: data.isDone,
+            id: data.id,
+            isEdit: false,
+          },
+        ];
+      });
+      setInputValue('');
+    } catch (error) {
+      console.error(error);
+    }
   }
 
-  function handleKeyDown() {
+  async function handleKeyDown() {
     if (inputValue.length === 0) {
       return;
     }
 
-    setTodos((prevTodos) => {
-      return [
-        ...prevTodos,
-        {
-          title: inputValue,
-          isDone: false,
-          id: Math.random() * 100,
-        },
-      ];
-    });
+    try {
+      const data = await createTodo({
+        title: inputValue,
+        idDone: false,
+      });
 
-    setInputValue('');
+      setTodos((prevTodos) => {
+        return [
+          ...prevTodos,
+          {
+            title: data.title,
+            isDone: data.isDone,
+            id: data.id,
+            isEdit: false,
+          },
+        ];
+      });
+      setInputValue('');
+    } catch (error) {
+      console.error(error);
+    }
   }
 
   function handleToggleDone(id) {
@@ -114,6 +111,22 @@ const TodoPage = () => {
     });
   }
 
+  useEffect(() => {
+    const getTodosAsync = async () => {
+      try {
+        const todos = await getTodos();
+
+        setTodos(
+          todos.map((todo) => {
+            return { ...todo, idEdit: false };
+          }),
+        );
+      } catch (error) {
+        console.error(error);
+      }
+    };
+    getTodosAsync();
+  });
   return (
     <div>
       TodoPage
