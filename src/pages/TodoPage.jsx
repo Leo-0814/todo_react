913576f607ae7c4ad9b045ff9e63f63b927e3a22
Leo <hank8142000@yahoo.com.tw>
2023-07-04 21:14:26 +0800

    second commit

---
+++ src/pages/TodoPage.jsx
@@ -0,0 +1,139 @@
+import { Footer, Header, TodoCollection, TodoInput } from 'components';
+import { useState } from 'react';
+
+const dummyTodos = [
+  {
+    title: 'Learn react-router',
+    isDone: true,
+    id: 1,
+  },
+  {
+    title: 'Learn to create custom hooks',
+    isDone: false,
+    id: 2,
+  },
+  {
+    title: 'Learn to use context',
+    isDone: true,
+    id: 3,
+  },
+  {
+    title: 'Learn to implement auth',
+    isDone: false,
+    id: 4,
+  },
+];
+
+const TodoPage = () => {
+  const [inputValue, setInputValue] = useState('');
+  const [todos, setTodos] = useState(dummyTodos);
+
+  function handleChange(value) {
+    setInputValue(value);
+  }
+
+  function handleAddTodo() {
+    if (inputValue.length === 0) {
+      return;
+    }
+
+    setTodos((prevTodos) => {
+      return [
+        ...prevTodos,
+        {
+          title: inputValue,
+          isDone: false,
+          id: Math.random() * 100,
+        },
+      ];
+    });
+
+    setInputValue('');
+  }
+
+  function handleKeyDown() {
+    if (inputValue.length === 0) {
+      return;
+    }
+
+    setTodos((prevTodos) => {
+      return [
+        ...prevTodos,
+        {
+          title: inputValue,
+          isDone: false,
+          id: Math.random() * 100,
+        },
+      ];
+    });
+
+    setInputValue('');
+  }
+
+  function handleToggleDone(id) {
+    setTodos((prevTodos) => {
+      return prevTodos.map((todo) => {
+        if (todo.id === id) {
+          return {
+            ...todo,
+            isDone: !todo.isDone,
+          };
+        } else {
+          return todo;
+        }
+      });
+    });
+  }
+
+  function handleChangeMode({ id, isEdit }) {
+    setTodos((prevTodos) => {
+      return prevTodos.map((todo) => {
+        if (todo.id === id) {
+          return { ...todo, isEdit: isEdit };
+        }
+
+        return { ...todo, isEdit: false };
+      });
+    });
+  }
+
+  function handleSave({ id, title }) {
+    setTodos((prevTodos) => {
+      return prevTodos.map((todo) => {
+        if (todo.id === id) {
+          return { ...todo, title: title, isEdit: false };
+        }
+        return { ...todo };
+      });
+    });
+  }
+
+  function handleDelete(id) {
+    setTodos((prevTodos) => {
+      return prevTodos.filter((todo) => todo.id !== id);
+    });
+  }
+
+  return (
+    <div>
+      TodoPage
+      <Header />
+      <TodoInput
+        inputValue={inputValue}
+        onChange={handleChange}
+        onAddTodo={handleAddTodo}
+        onKeyDown={handleKeyDown}
+      />
+      <TodoCollection
+        todos={todos}
+        onToggleDone={handleToggleDone}
+        onChangeMode={handleChangeMode}
+        onSave={handleSave}
+        onDelete={handleDelete}
+      />
+      <Footer todos={todos} />
+    </div>
+  );
+};
+
+export default TodoPage;
