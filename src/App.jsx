4696602ddcc9ac94f83c5027b0c30f39e27ffd17
Leo <hank8142000@yahoo.com.tw>
2023-07-10 09:54:52 -0300

    logout && authContext

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import { AuthProvider } from 'Context/authContext';
 import './App.scss';
 import { HomePage, TodoPage, LoginPage, SignUpPage } from './pages';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
@@ -6,12 +7,14 @@ function App() {
   return (
     <div className="app">
       <BrowserRouter>
-        <Routes>
-          <Route path="login" element={<LoginPage />} />
-          <Route path="signup" element={<SignUpPage />} />
-          <Route path="todos" element={<TodoPage />} />
-          <Route path="*" element={<HomePage />} />
-        </Routes>
+        <AuthProvider>
+          <Routes>
+            <Route path="login" element={<LoginPage />} />
+            <Route path="signup" element={<SignUpPage />} />
+            <Route path="todos" element={<TodoPage />} />
+            <Route path="*" element={<HomePage />} />
+          </Routes>
+        </AuthProvider>
       </BrowserRouter>
     </div>
   );
